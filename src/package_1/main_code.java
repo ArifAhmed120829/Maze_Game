3e1e9b0778611d5028c933524726739a461bce26
ArifAhmed <arif15-12301@diu.edu.bd>
2023-12-29 23:18:13 +0600

    nothing
    sadad

--- src/package_1/main_code.java
+++ src/package_1/main_code.java
@@ -3,7 +3,7 @@ package package_1;
 public class main_code {
 
 	public static void main(String[] args) {
-		System.out.println("hello");
+		
 	}
 
 }
---
+++ src/package_1/tree.java
@@ -0,0 +1,64 @@
+package package_1;
+
+public class tree {
+	static class Node{
+		int data;
+		Node left;
+		Node right;
+		Node(int data){
+			this.data = data;
+		}
+	}
+	static class linked{
+		Node head;
+		Node root;
+		
+		void push(int d) {
+			head = traverse(head,d);
+		}
+		Node traverse(Node head, int val) {
+			if(head != null && root == null) root = head;
+			if(head == null) return new Node(val);
+			else if(head.data>=val) head.left = traverse(head.left,val);
+			else if(head.data<=val) head.right = traverse(head.right,val);
+			return head;
+		}
+	}
+	public static void main(String[] args) {
+		int[] arr = {8,4,10,2,6,20};
+		linked ll = new linked();
+		for(int val:arr) ll.push(val);
+		show(ll.root);
+
+	}
+	//In order
+	/*
+	private static void show(Node root) {
+		if(root != null) {
+			show(root.left);
+			System.out.println(root.data);
+			show(root.right);
+		}
+		
+	}*/
+	//Pre-order
+	/*
+		private static void show(Node root) {
+			if(root != null) {
+				System.out.println(root.data);
+				show(root.left);
+				show(root.right);
+			}
+			
+		}*///
+	//Post-order
+	private static void show(Node root) {
+		if(root != null) {
+			show(root.left);
+			show(root.right);
+			System.out.println(root.data);
+		}
+		
+	}
+
+}
