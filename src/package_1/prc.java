05b3386152167f021f228f77a8e763db7794b2a7
ArifAhmed <arif15-12301@diu.edu.bd>
2024-03-01 17:50:59 +0600

    added prc class

---
+++ src/package_1/prc.java
@@ -0,0 +1,17 @@
+package package_1;
+import java.util.*;
+
+
+public class prc {
+
+	public static void main(String[] args) {
+		ArrayList<ArrayList<Integer>> ls = new ArrayList<>();
+		for(int i = 1; i<10; i++) {
+			ls.add(new ArrayList<>());
+		}
+		System.out.println(ls.get(0));
+		
+		
+	}
+
+}
