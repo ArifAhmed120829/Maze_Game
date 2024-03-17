3725175a1e2dda8ab408bee6627c82365cb0b10a
ArifAhmed <arif15-12301@diu.edu.bd>
2024-03-17 15:12:17 +0600

    inserted input file

--- src/package_1/main_code.java
+++ src/package_1/main_code.java
@@ -1,10 +1,25 @@
 package package_1;
 import java.util.*;
 
+
 public class main_code {
+	static class solution{
+		public int nearestExit(char[][] maze, int[] entrance) {
+			return 0;
+			
+		}
+	}
 
 	public static void main(String[] args) {
-		System.out.println("whats'up");
+		
+		char[][] maze = {{'+','+','.','+'},{'.','.','.','+'},{'+','+','+','.'}};
+		
+		int[] entrance = {1,2};
+				
+		solution s = new solution();
+		
+		//number of steps in the shortest path from entrance to exit
+		System.out.println(s.nearestExit(maze, entrance));
 		
 		
 	}
