aa98ad6785ad0f6e750a3d8f96eb41195d3156f9
ArifAhmed <arif15-12301@diu.edu.bd>
2024-03-18 15:03:44 +0600

    solving a bfs problem

---
+++ src/package_1/helpClotilde.java
@@ -0,0 +1,26 @@
+package package_1;
+import java.util.*;
+//beecrowd problem no 1910
+
+public class helpClotilde {
+
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		int s,d,e,e1,e2;
+		
+		for(;;) {
+			 s = in.nextInt();
+			 d = in.nextInt();
+			 e = in.nextInt();
+			 
+			 if(s==0 && d == 0 && e == 0) break;
+			 
+			 for(int i =0; i<e; i++) {
+				 e1 = in.nextInt();
+				 e2 = in.nextInt();
+			 }
+			 
+		}
+	}
+
+}
