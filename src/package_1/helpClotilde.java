e1ab909993d985ace003ba0240aa9d20cff5dced
ArifAhmed <arif15-12301@diu.edu.bd>
2024-03-22 17:10:21 +0600

    added list of integer files

--- src/package_1/helpClotilde.java
+++ src/package_1/helpClotilde.java
@@ -6,19 +6,27 @@ public class helpClotilde {
 
 	public static void main(String[] args) {
 		Scanner in = new Scanner(System.in);
-		int s,d,e,e1,e2;
+		int s,d,e,e1;
+		ArrayList<ArrayList<Integer>> dlist = new ArrayList<>();
+		ArrayList<Integer> l = new ArrayList<>();
 		
 		for(;;) {
-			 s = in.nextInt();
-			 d = in.nextInt();
-			 e = in.nextInt();
+			 s = in.nextInt();//3
+			 d = in.nextInt();//8
+			 e = in.nextInt();//2
 			 
 			 if(s==0 && d == 0 && e == 0) break;
 			 
 			 for(int i =0; i<e; i++) {
-				 e1 = in.nextInt();
-				 e2 = in.nextInt();
+				 e1 = in.nextInt();//4,5
+				 l.add(e1);
 			 }
+			 dlist.add(l);
+			 System.out.println("Total "+dlist.size()+" List");
+			 int x = 0;
+			 System.out.println("Number of elements is " +dlist.get(x).size());
+			 x++;
+			 
 			 
 		}
 	}
