28efaba3544b3263f463e36373dcaf5c7ab28199
ArifAhmed <arif15-12301@diu.edu.bd>
2024-03-23 15:12:26 +0600

    added graph

--- src/package_1/helpClotilde.java
+++ src/package_1/helpClotilde.java
@@ -3,6 +3,34 @@ import java.util.*;
 //beecrowd problem no 1910
 
 public class helpClotilde {
+	static class Edge{//a class to store graph edge
+		int source, dest;
+		public Edge(int source, int dest) {
+			this.source = source;
+			this.dest =dest;
+		}
+	}
+	//a class to represent graph object
+	static class Graph{
+		// a list of list to represent an adjacency list
+		ArrayList<ArrayList<Integer>> adjlist = null;
+		Graph(ArrayList<Edge> edges,int n){
+			adjlist = new ArrayList<>();
+			for(int i = 0; i<n; i++) {
+				adjlist.add(new ArrayList<>());	
+				}
+			//add edges to the undirected graph
+			for(Edge edge: edges) {
+				int src = edge.source;
+				int dest = edge.dest;
+				
+				adjlist.get(src).add(dest);
+				adjlist.get(dest).add(src);
+			}
+		}
+		
+	}
+	
 
 	public static void main(String[] args) {
 		Scanner in = new Scanner(System.in);
@@ -28,6 +56,27 @@ public class helpClotilde {
 			 x++;
 			 
 			 
+			 
+		}
+	}
+	public static void BFS(Graph graph,int v, boolean[] discovered) {
+		//create a queue for doing bfs
+		Queue<Integer> q = new ArrayDeque<>();
+		//mark the source vertex as discovered
+		discovered[v] = true;
+		q.add(v);
+		while(!q.isEmpty()) {
+			v= q.poll();
+			System.out.print(v+" ");
+			
+			//do for every edge(v,u)
+			for(int u: graph.adjlist.get(v)) {
+				if(!discovered[u]) {
+					discovered[u] = true;
+					q.add(u);
+				}
+			}
+			
 		}
 	}
 
