2126a9e0c87ec69a5673227fce12fd6be0539852
ArifAhmed <arif15-12301@diu.edu.bd>
2024-05-03 16:28:18 +0600

    added new file

---
+++ src/package_1/helpcolitide2.java
@@ -0,0 +1,31 @@
+package package_1;
+import java.util.*;
+
+public class helpcolitide2 {
+
+	public static void main(String[] args) {
+		Queue<Integer> queue1 = new ArrayDeque<>();
+		Queue<Integer> queue2 = new ArrayDeque<>();
+		Queue<Integer> queue3 = new ArrayDeque<>();
+		Queue<Integer> queue4 = new ArrayDeque<>();
+		Queue<Integer> queue5 = new ArrayDeque<>();
+		
+		int[] arr = {3,8};
+		ArrayList<Integer> bag = new ArrayList<>();
+		bag.add(4);
+		bag.add(5);
+		ArrayList<Integer> list = new ArrayList<>();
+		list.add(-1);//-1
+		list.add(1);//+1
+		list.add(2);// /2
+		list.add(2);// *2
+		list.add(3);// *3
+		
+		//every queue belongs to children from list ** may 3, 16:23
+		
+		
+		
+		
+	}
+
+}
