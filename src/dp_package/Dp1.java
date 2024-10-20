0a2c7055e9a1f2d0de4f4d864d3b16253a1244f1
ArifAhmed <arif15-12301@diu.edu.bd>
2024-10-20 14:12:11 +0600

    wip

--- src/dp_package/Dp1.java
+++ src/dp_package/Dp1.java
@@ -1,12 +1,26 @@
 package dp_package;
 
 public class Dp1 {
-
-	public static void main(String[] args) {
-		System.out.println("je");
-		System.out.println("sawdawd");
+// naive approach vs expected approach
+	public static void main(String[] args) { // 8th fib = 21
+		//naive approach - recursion
+		int n  = 15;
+		long st = System.nanoTime();
+		System.out.println(rec(n));
+		long sp = System.nanoTime();
+		long ms = (sp - st) / 1000000;
+		System.out.println(ms + " ms");
+		
+		
 		
 
+	}
+	static int rec(int x) {
+		if(x == 1) return 1;
+		if(x==0) return 0;
+		
+		return rec(x-1) + rec(x-2);
+		
 	}
 
 }
