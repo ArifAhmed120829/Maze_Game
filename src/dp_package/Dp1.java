36fcf2856877b604134f185a74239e01f2539278
ArifAhmed <arif15-12301@diu.edu.bd>
2024-10-23 02:51:35 +0600

    wip

--- src/dp_package/Dp1.java
+++ src/dp_package/Dp1.java
@@ -4,17 +4,36 @@ public class Dp1 {
 // naive approach vs expected approach
 	public static void main(String[] args) { // 8th fib = 21
 		//naive approach - recursion
-		int n  = 15;
+		int n  = 25;
 		long st = System.nanoTime();
 		System.out.println(rec(n));
 		long sp = System.nanoTime();
 		long ms = (sp - st) / 1000000;
 		System.out.println(ms + " ms");
+		long st2 = System.nanoTime();
+		bua(n);//bottom up approach
+		long sp2 = System.nanoTime();
+		long ms2 = (sp2 - st2) / 1000000;
+		System.out.println(ms2 + " ms");
 		
+		//one is 2 ms and another is 0 ms
+
 		
 		
 
 	}
+	private static void bua(int n) {
+		
+		int x = 0;
+		int y = 1;
+		
+		for(int i = 1; i<n; i++) {
+			int temp = x + y;
+			x = y;
+			y = temp;
+		}
+		System.out.println(y);
+}
 	static int rec(int x) {
 		if(x == 1) return 1;
 		if(x==0) return 0;
